2ec8efd9eebacb6818e4b4e6931ab1df54b62039
aranzazu.ab <a.bremauntz.aranzazu@gmail.com>
2026-06-10 23:34:59 +0200

    feat: create Main

--- src/main/java/sprint01/exe01/Main.java
+++ src/main/java/sprint01/exe01/Main.java
@@ -1,6 +1,13 @@
 package sprint01.exe01;
 
+public class Main {
+    static void main() {
+        Instrument wind01 = new WindInstrument("saxophone", 85.00);
+        Instrument string01 = new StringInstrument("violin", 140.00);
+        Instrument percussion01 = new PercussionInstrument("drums", 250.00);
 
-        }
+        wind01.play();
+        string01.play();
+        percussion01.play();
     }
 }
