a53e670384b93fb7fb2b093eceaa1b18e3dc0e8b
aranzazu.ab <a.bremauntz.aranzazu@gmail.com>
2026-06-10 23:27:22 +0200

    feat: create subclass PercussionInstrument

---
+++ src/main/java/sprint01/exe01/PercussionInstrument.java
@@ -0,0 +1,13 @@
+package sprint01.exe01;
+
+public class PercussionInstrument extends Instrument {
+
+    public PercussionInstrument (String name, double price) {
+        super (name, price);
+    }
+
+    @Override
+    public void play(){
+        System.out.println("It's playing a percussion instrument");
+    }
+}
