512dab27ac71d97fae9663ac60174d013174cbf1
aranzazu.ab <a.bremauntz.aranzazu@gmail.com>
2026-06-10 23:15:42 +0200

    feat: create subclass StringInstrument

---
+++ src/main/java/sprint01/exe01/StringInstrument.java
@@ -0,0 +1,13 @@
+package sprint01.exe01;
+
+public class StringInstrument extends Instrument {
+
+    public StringInstrument (String name, double price) {
+        super (name, price);
+    }
+
+    @Override
+    public void play(){
+        System.out.println("It's playing a string instrument");
+    }
+}
