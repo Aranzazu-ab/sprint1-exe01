50ecebe6b07b32a0369e771e440b1bcd5681d148
aranzazu.ab <a.bremauntz.aranzazu@gmail.com>
2026-06-11 00:08:12 +0200

    feat: add static block and static atribut to Instrument class

--- src/main/java/sprint01/exe01/Instrument.java
+++ src/main/java/sprint01/exe01/Instrument.java
@@ -4,9 +4,16 @@ public abstract class Instrument {
     private String name;
     private double price;
 
+    public static int total = 0;
+
+    static{
+        System.out.println("Class Instrument is loaded.");
+    }
+
     public Instrument(String name, double price){
         this.name = name;
         this.price=price;
+        total++;
     }
 
     public String getName() {
